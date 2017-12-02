0ef7061b56ec670a3cb20283557b87d7e37886d6
ImHype <543717080@qq.com>
2017-12-02 12:33:41 +0800

    feat: Add inject condition

--- src/shared/decorators/injection.ts
+++ src/shared/decorators/injection.ts
@@ -5,7 +5,12 @@ import { isUndefined, isString, isNil } from '../index';
 
 function inject(key: string) {
     return function (service: any) {
+        if (isNil(service)) {
+            throw new Error('请确保注入操作传入和非空的类');
+        }
+
         return function(target: any, propertyKey?: string | symbol) {
+            
             let services = Reflect.getMetadata(key, target);
             if (isNil(services)) {
                 services = new Map();
