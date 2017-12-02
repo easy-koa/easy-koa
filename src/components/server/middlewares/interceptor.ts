50f0dcbcb0a92f7075d1c95bd344f9d7c36a8dc7
ImHype <543717080@qq.com>
2017-12-02 12:40:39 +0800

    chore: try using import * as xxx instead of require

--- src/components/server/middlewares/interceptor.ts
+++ src/components/server/middlewares/interceptor.ts
@@ -1,11 +1,11 @@
-import Koa = require('koa');
+import { Context } from 'koa';
 import { isUndefined } from '../../../shared/index';
 import { InterceptorMapping } from '../interfaces/interceptor';
 
 
 export function interceptorMiddleware(interceptorMapping: InterceptorMapping) {
-    return async function (ctx: Koa.Context, next: Function) {
-        const { path, methods, interceptor } = interceptorMapping;
+    const { path, methods, interceptor } = interceptorMapping;
+    return async function (ctx: Context, next: Function) {
         const pathReg = <RegExp> path;
 
         if (!pathReg.test(ctx.path)) {
