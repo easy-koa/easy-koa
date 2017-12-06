e2dffd1ff2c0a0d326d12da4578be67cef60134b
ImHype <543717080@qq.com>
2017-12-06 11:10:08 +0800

    chore: bootstrap both in src and dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 gulp.task('cp', function () {
+    gulp.src('./lerna.json').pipe(gulp.dest('./dist'));
     gulp.src('./packages/*/package.json').pipe(gulp.dest('./dist/packages'))
     gulp.src('./examples/*/package.json').pipe(gulp.dest('./dist/examples'))
 })
