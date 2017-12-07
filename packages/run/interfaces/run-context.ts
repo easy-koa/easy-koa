51a9b1274a8d13db2390399d4064c300d771705e
ImHype <543717080@qq.com>
2017-12-07 20:01:08 +0800

    chore(fix): modify path

--- packages/run/interfaces/run-context.ts
+++ packages/run/interfaces/run-context.ts
@@ -2,7 +2,7 @@ import { ServerOptions, ServerContext } from "@kaola/kapp-server/interfaces";
 
 import { LoggerOptions } from "@kaola/kapp-logger/interfaces/logger-options";
 import { MonitorOptions } from "@kaola/kapp-monitor/interfaces/index";
-import { Component } from "../../core/index";
+import { Component } from "@kaola/core/index";
 
 export interface ModuleContext {
     components: Component[];
