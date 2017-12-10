71cd75c70f35c37d6443ec3ccabc2b18823ee669
ImHype <543717080@qq.com>
2017-12-10 21:41:38 +0800

    feat: Add error collection in forwarder plugin

--- packages/forwarder/index.ts
+++ packages/forwarder/index.ts
@@ -1,11 +1,12 @@
-import { ServerRequest, ServerResponse, Agent, IncomingMessage, OutgoingHttpHeaders } from 'http';
-import { Component } from '@kaola/kapp-core';
-import { Koa, startTime } from '@kaola/kapp-shared';
-import { InjectPlugin } from '@kaola/kapp-shared';
-import { Monitor } from '@kaola/kapp-monitor';
-import { ProxyServer } from './proxy-server';
-import { ProxyServerConfig } from './interfaces';
-import * as createMonitorPlainObject from './utils/create-monitor-plain-object';
+import { ServerRequest, ServerResponse, Agent, IncomingMessage, OutgoingHttpHeaders } from 'http'
+import { Component } from '@kaola/kapp-core'
+import { Koa, startTime } from '@kaola/kapp-shared'
+import { InjectPlugin } from '@kaola/kapp-shared'
+import { Monitor } from '@kaola/kapp-monitor'
+import { ProxyServer } from './proxy-server'
+import { ProxyServerConfig } from './interfaces'
+import * as createMonitorPlainObject from './utils/create-monitor-plain-object'
+
 
 declare module 'http' {
     interface ServerResponse {
@@ -14,60 +15,70 @@ declare module 'http' {
 }
 
 export class Forwarder extends Component {
-    private proxyServer: ProxyServer;
+    private proxyServer: ProxyServer
 
     @InjectPlugin(Monitor)
-    private monitor: Monitor;
+    private monitor: Monitor
 
-    name() {
-        return 'forward';
+    name(): string {
+        return 'forward'
     }
 
     constructor(options: ProxyServerConfig) {
-        super();
-        this.$options = options;
+        super()
+        this.$options = options
     }
 
-    async init() {
-        const { host, specialHeader, secure = false, agent = false, proxyTimeout = 3000 } = this.$options;
+    async init(): Promise<void> {
+        const { host, specialHeader, secure = false, agent = false, proxyTimeout = 3000 } = this.$options
         this.proxyServer = ProxyServer.create({
             host,
             specialHeader,
             secure,
             agent,
-            proxyTimeout
-        });
+            proxyTimeout,
+        })
+
+        this.proxyServer.on('error', (error: Error) => {
+            this.monitor.collectError({
+                type: 'error',
+                payload: {
+                    stack: error && error.stack,
+                    message: error && error.message,
+                },
+            })
+        })
     }
 
-    async forward (ctx: Koa.Context ) {
-        let end = startTime();
-        const { target } = this.$options;
+    async forward(ctx: Koa.Context): Promise<{ body: any, header: any}> {
+        const end: Function = startTime()
+        const { target } = this.$options
 
-        const responseBody: any[] = [];
+        const responseBody: any[] = []
 
-        const req = ctx.req;
-        const res = new ServerResponse(req);
-        
-        res.write = function (chunk: Buffer) {
-            responseBody.push(chunk);
-            return true;
-        };
+        const req: IncomingMessage = ctx.req
+        const res: ServerResponse = new ServerResponse(req)
 
-        this.proxyServer.web(req, res, { target });
+        res.write = function (chunk: Buffer): boolean {
+            responseBody.push(chunk)
+            return true
+        }
 
-        await new Promise((resolve) => {
-            res.once('proxyed', () => resolve());
-        });
+        this.proxyServer.web(req, res, { target })
 
-        this.monitor.collect( createMonitorPlainObject.forward({
+        await new Promise((resolve: Function): void => {
+            res.once('proxyed', () => resolve())
+        })
+
+        this.monitor.collect(createMonitorPlainObject.forward({
             action: ctx.path,
             time: end(),
-            status: res.statusCode
-        }) )
+            status: res.statusCode,
+        }))
 
         return {
             body: Buffer.concat(responseBody),
-            header: res._headers
-        };
+            header: res._headers,
+        }
     }
 }
--- packages/monitor/index.ts
+++ packages/monitor/index.ts
@@ -1,56 +1,53 @@
-import { Component } from "@kaola/kapp-core";
-import { InjectPlugin, Koa } from "@kaola/kapp-shared";
-import { Logger } from "@kaola/kapp-logger";
-import { MonitorOptions } from "./interfaces/index";
-import { BaseObject } from "@kaola/kapp-shared";
-import { setInterval } from "timers";
-import { Cron } from "@kaola/kapp-cron";
-import { MonitorLogger } from "./interfaces/monitor-logger";
-
+import { Component } from "@kaola/kapp-core"
+import { InjectPlugin, Koa } from "@kaola/kapp-shared"
+import { Logger } from "@kaola/kapp-logger"
+import { MonitorOptions } from "./interfaces/index"
+import { BaseObject } from "@kaola/kapp-shared"
+import { setInterval } from "timers"
+import { Cron } from "@kaola/kapp-cron"
+import { MonitorLogger } from "./interfaces/monitor-logger"
 
 
 export class Monitor extends Component {
     @InjectPlugin(Cron)
-    private cron: Cron;
+    private cron: Cron
 
     @InjectPlugin(Logger)
-    private logger: Logger;
+    private logger: Logger
 
-    private monitor: MonitorLogger;
+    private monitor: MonitorLogger
 
-    private traceMap = new Map()
-
-    name() {
-        return 'monitor';
+    name(): string {
+        return 'monitor'
     }
 
     constructor(...args: any[]) {
-        super();
+        super()
     }
 
-    init() {
-        this.monitor = this.logger.create(`kapp-${this.name()}`);
+    init(): void {
+        this.monitor = this.logger.create(`kapp-${this.name()}`)
     }
 
     collect(message: any, ctx?: Koa.Context): void {
         if (message) {
-            this.monitor.info(this.createCollection(message, ctx));
+            this.monitor.info(this.createCollection(message, ctx))
         }
     }
 
     collectError(message: any, ctx?: Koa.Context): void {
         if (message) {
-            this.monitor.error(this.createCollection(message, ctx));
+            this.monitor.error(this.createCollection(message, ctx))
         }
     }
 
-    createCollection(payload: any, ctx: Koa.Context) {
-        if ( payload ) {
-            return JSON.stringify(payload);
+    createCollection(payload: any, ctx: Koa.Context): void | string {
+        if (payload) {
+            return JSON.stringify(payload)
         }
     }
 
-    static configure(options: MonitorOptions) {
-        return {};
+    static configure(options: MonitorOptions): BaseObject {
+        return {}
     }
 }
